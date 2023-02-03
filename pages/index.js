11d4fde6512a0761d1feece7e8e6495808fade69
Mallikarjuna Annigeri <mallikarjunaannigeri27@gmail.com>
2023-02-03 10:31:42 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 const getSomething = () => (
   <>
     <p>Hi, next.js</p>
-
     <Link href={"/about"}>About us</Link>
     <Link href={"/courses"}>Courses page</Link>
   </>
