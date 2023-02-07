8ead3f7f57b93a60313689016dcc01c178e4f398
Mallikarjuna Annigeri <mallikarjunaannigeri27@gmail.com>
2023-02-07 11:10:39 +0530

    navigating

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const Home = () => {
-  return <h2>Home page</h2>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/product");
+  };
+
+  return (
+    <div>
+      <h2>Home page</h2>
+      <Link href="/blogs">Blog page</Link>
+      <Link href="/product">Product page</Link>
+
+      <button onClick={handleClick}>Place order</button>
+    </div>
+  );
 };
 
 export default Home;
--- nextjs-blog/pages/product/index.js
+++ nextjs-blog/pages/product/index.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 function productList() {
   return (
     <>
-      <h2>product 1</h2>
-      <h2>product 2</h2>
-      <h2>product 3</h2>
+      <h2>
+        <Link href="/product/1">product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/product/3" replace>
+          product 3
+        </Link>
+      </h2>
+      <Link href="/">Go Home</Link>
     </>
   );
 }
