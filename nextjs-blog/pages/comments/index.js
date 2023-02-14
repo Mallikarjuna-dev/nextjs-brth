6bef7305646d9e93818ad1836403eb2b5bfbed30
Mallikarjuna Annigeri <mallikarjunaannigeri27@gmail.com>
2023-02-14 09:59:42 +0530

    api post and delete requests

--- nextjs-blog/pages/comments/index.js
+++ nextjs-blog/pages/comments/index.js
@@ -2,19 +2,52 @@ import { useState } from "react";
 
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
 
   const handleCom = async () => {
     const res = await fetch("api/comments");
     const data = await res.json();
     setComments(data);
   };
+
+  const handleSubmit = async () => {
+    const res = await fetch("api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  const handleComment = async (commentId) => {
+    const res = await fetch(`api/comments/${commentId}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    console.log(data);
+    handleCom();
+  };
+
   return (
     <>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Save Changes</button>
+
       <button onClick={handleCom}>Load comments</button>
       {comments.map((comment) => {
         return (
           <div key={comment.id}>
-            <h2>{comment.text}</h2>
+            <p>
+              {comment.id} {comment.text}
+              <button onClick={() => handleComment(comment.id)}>Delete</button>
+            </p>
           </div>
         );
       })}
